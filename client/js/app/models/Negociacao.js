ddd74d6652a08cf01545155eabf8f6281574e7dc
Yuri <yurifdev@gmail.com>
2022-08-15 21:46:48 -0300

    Apllying getters into the negociation

--- client/js/app/models/Negociacao.js
+++ client/js/app/models/Negociacao.js
@@ -2,19 +2,42 @@
 
 class Negociacao {
     constructor(data, quantidade, valor){
-        this.data = data;
-        this.quantidade = quantidade;
-        this.valor = valor;
+        //OBS: Sei que o JS oferece essa nova funcionalidade, mas preferi seguir o curso.
+        //P.S: I know that i can do like this nowadays, but i chose to fallow what professor is teaching
+        // this.#data = data;
+        // this.#quantidade = quantidade;
+        // this.#valor = valor;
+
+        //Por convenção, nunca se deve alterar propriedades com o underline na frente
+        this._data = data;
+        this._quantidade = quantidade;
+        this._valor = valor;
 
     }
 
     //Função = Função fora da classe
     //Método = Função dentro da classe
 
-    obtemVolume() {
-        return this.quantidade * this.valor;
-        
+    // Teoricamente, é um método. Mas eu não preciso colocar parenteses nele. Só preciso chamar 'var n1 = new Negociacao || n1.volume'
+    // getvolume() {
+    get volume() {
+        return this._quantidade * this._valor;
+
     }
 
+    get data() {
+        return this._data;
+
+    }
+
+    get quantidade() {
+        return this._quantidade;
+
+    }
+
+    get valor() {
+        return this._valor;
+
+    }
 
 }
