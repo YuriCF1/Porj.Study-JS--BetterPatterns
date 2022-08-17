535b37dea4afb0dda95e3ef5aea1198e949f3003
Yuri <yurifdev@gmail.com>
2022-08-17 20:35:53 -0300

    Creating the DateHelper class to format the Date value

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -10,19 +10,36 @@ class NegociacaoController {
 
   adiciona(event) {
     event.preventDefault();
+    
+    let helper = new DateHelper();
 
-    let data = new Date(this._inputData.value.split('-')); //Seperando o valor de string é um array(new Date(['2022','15','08'])), tomando o '-' da string como parâmetro.
+    // // ... = Spread Operator. Desmembra o array. Primeiro item do array, sendo primeiro parâmetro do Date, e etc.
+    // let data = new Date(
+    //   ...this._inputData.value
+    //     .split("-") //Seperando o valor de string é um array(new Date(['2022','15','08'])), tomando o '-' da string como parâmetro.
+    //     //Compensando o valor do mês
+    //     .map((item, indice) => item - indice % 2) //Arrow function já dá o retorno por padrão
+      
+    //     // { // Usando function
+    //       // return item - indice % 2; // No indice 1(mes), dividido por 2, fica 0.5. Então o módulo arredonda o resto. Sendo 1, então fica o valor do item menos 1. Evitando um IF
+          
+    //       //Forma mais verbosa
+    //       // if (indice == 1) { 
+    //         // return item - 1;
+    //       // }
+    //       // return item;
+    //     // })
+    // );
+
     // let data = new Date(this._inputData.value.replace(/-/g,',')); //Também funciona
-    console.log(data);
-
-    console.log(typeof this._inputData.value);
-    console.log(this._inputData.value);
-
     let negociacao = new Negociacao(
-      this._inputData.value,
+      helper.textoParaData(this._inputData.value),
       this._inputQuantidade.value,
       this._inputValor.value
     );
+
     console.log(negociacao);
+    console.log(helper.dataParaTexto(negociacao.data));
+
   }
 }
