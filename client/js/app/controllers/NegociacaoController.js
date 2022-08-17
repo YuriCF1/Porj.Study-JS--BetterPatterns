2d5cfd003021d1e6b75eff9022e335f68c25e2cf
Yuri <yurifdev@gmail.com>
2022-08-17 11:03:10 -0300

    Making a copy of Date value for safety

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -1,18 +1,28 @@
 class NegociacaoController {
+  constructor() {
+    //Se eu deixar isso fora do constructor, toda vez que clicar para importar, o JS vai percorrer a DOM, por isso é bom colocar no constructor para evitar isso.
+    let $ = document.querySelector.bind(document); //O this devia ser o document, mas já que salvei o método no $, o bind serve para associar ainda o this ao '(document)'
 
-    adiciona(event) {
-        event.preventDefault()
+    this._inputData = $("#data");
+    this._inputQuantidade = $("#quantidade");
+    this._inputValor = $("#valor");
+  }
 
-        let $ = document.querySelector.bind(document); //O this devia ser o document, mas já que salvei o método no $, o bind serve para associar ainda o this ao '(document)'
-        // let inputData = document.querySelector("#data")
-        // let inputQuantidade = document.querySelector("#quantidade")
-        // let inputValor = document.querySelector("#valor")
-        let inputData = $("#data")
-        let inputQuantidade = $("#quantidade")
-        let inputValor = $("#valor")
+  adiciona(event) {
+    event.preventDefault();
 
-        console.log(inputData.value);
-        console.log(inputQuantidade.value);
-        console.log(inputValor.value);
-    };
-};
+    let data = new Date(this._inputData.value.split('-')); //Seperando o valor de string é um array(new Date(['2022','15','08'])), tomando o '-' da string como parâmetro.
+    // let data = new Date(this._inputData.value.replace(/-/g,',')); //Também funciona
+    console.log(data);
+
+    console.log(typeof this._inputData.value);
+    console.log(this._inputData.value);
+
+    let negociacao = new Negociacao(
+      this._inputData.value,
+      this._inputQuantidade.value,
+      this._inputValor.value
+    );
+    console.log(negociacao);
+  }
+}
